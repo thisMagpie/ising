49fd6eab254d717caf59234efa57a290c8002235
Magdalen Berns <m.berns@thismagpie.com>
2015-02-06 11:51:24 +0000

    remove whitespace

--- IO.java
+++ IO.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2012-2015  Magdalen Berns
+ * Copyright (C) 2012-2015 Magdalen Berns
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
